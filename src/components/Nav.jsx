a18b139e63b31dfb99a9ba29f60b68b8257ba114
Ava Blake <avablakedesign@protonmail.com>
2023-01-29 14:51:04 -0500

    updated Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,30 +1,46 @@
 import {useState} from "react"
-import {Link} from "react-router-dom"
+import {Link, useLocation} from "react-router-dom"
 const Nav = () => {
-    const [selectedLink, setSelectedLink] = useState("aboutme");
+    const location = useLocation()
+    // This is taking the pathname from location and using the value without a slash as the default state.
+    const [selectedLink, setSelectedLink] = useState(location.pathname.slice(1));
+    const renderNavLinks = () => {
+      const navLinks = [
+        {
+          linkName: "contact",
+          to: "/contact",
+          linkText: "Contact"
+        },
+        {
+          linkName: "aboutme",
+          to: "/aboutme",
+          linkText: "About Me"
+        },
+        {
+          linkName : "portfolio",
+          to: "/portfolio",
+          linkText: "Portfolio"
+        },
+        {
+          linkName: "resume",
+          to: "/resume",
+          linkText: "Resume"
+        }
+      ];
+      return navLinks.map((item)=>{
+        return(
+          <li onClick = {()=>setSelectedLink(item.linkName)} className = {selectedLink === item.linkName ? "link-selected" : "link"} key={item.linkName}>
+            <Link to={item.to}>
+              {item.linkText}
+            </Link>    
+          </li>
+        )
+      })
+    }
     return (
         <nav>
             <ul>
-              <li onClick = {()=>setSelectedLink("contact")} className={selectedLink === "contact" ? "link-selected" : "link"}>
-                <Link to="/contact">
-                  Contact
-                </Link>    
-              </li>
-              <li onClick = {()=>setSelectedLink("aboutme")} className={selectedLink === "aboutme" ? "link-selected" : "link"}>
-                <Link to="/aboutme">
-                  About Me
-                </Link>
-              </li> 
-              <li onClick = {()=>setSelectedLink("portfolio")} className={selectedLink === "portfolio" ? "link-selected" : "link"}>
-                <Link to="/portfolio">
-                  Portfolio
-                </Link>
-              </li> 
-              <li onClick = {()=>setSelectedLink("resume")} className={selectedLink === "resume" ? "link-selected" : "link"}>
-              <Link to="/resume">
-                Resume
-              </Link>
-              </li>
+              {renderNavLinks()}
             </ul>  
         </nav>
     )
